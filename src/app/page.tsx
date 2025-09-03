2f54f535625caf721a25dfe04bf8e9c4c3f2d062
Ricardo <ricardoribeiropro4@gmail.com>
2025-09-02 21:09:10 -0300

    atualização do front-end

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,28 @@ import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Separator } from "@/components/ui/separator";
-import { Plus, List, Check, ArrowDownRight, SquarePen, Trash } from "lucide-react";
+import {
+  Plus,
+  List,
+  Check,
+  ArrowDownRight,
+  SquarePen,
+  Trash,
+  ListCheck,
+  Trash2,
+  Sigma,
+} from "lucide-react";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Dialog , DialogTrigger , DialogContent , DialogHeader , DialogTitle } from "@/components/ui/dialog";
 
 const Home = () => {
   return (
@@ -20,53 +41,114 @@ const Home = () => {
           <Separator className="mb-4" />
 
           <div className="flex gap-2">
-            <Badge className="cursor-pointer">
-              <List /> Todas
+            <Badge className="cursor-pointer" variant="default">
+              <List />
+              Todas
             </Badge>
-            <Badge className="cursor-pointer">
-              <ArrowDownRight /> Não Finalizadas
+            <Badge className="cursor-pointer" variant="outline">
+              <ArrowDownRight />
+              Não Finalizadas
             </Badge>
-            <Badge className="cursor-pointer">
-              <Check /> Concluidas
+            <Badge className="cursor-pointer" variant="outline">
+              <Check />
+              Concluidas
             </Badge>
           </div>
 
           <div className="mt-4 border-b-1">
-
             <div className="h-14 flex justify-between items-center border-t-1">
               <div className="w-1 h-full bg-green-300"></div>
               <p className="flex-1 px-2 text-sm">Estudar React</p>
               <div className="flex gap-2 items-center">
-                <SquarePen size={16} className="cursor-pointer" /> <Trash size={16} className="cursor-pointer" />{" "}
+                <SquarePen size={16} className="cursor-pointer" />{" "}
+                <Trash size={16} className="cursor-pointer" />{" "}
               </div>
             </div>
-
-            <div className="h-14 flex justify-between items-center border-top-1">
-              <div className="w-1 h-full bg-green-300"></div>
-              <p className="flex-1 px-2 text-sm">Estudar React</p>
-              <div className="flex gap-2 items-center">
-                <SquarePen size={16} className="cursor-pointer" /> <Trash size={16} className="cursor-pointer" />{" "}
-              </div>
-            </div>
-
-            <div className="h-14 flex justify-between items-center border-top-1">
-              <div className="w-1 h-full bg-green-300"></div>
-              <p className="flex-1 px-2 text-sm">Estudar React</p>
-              <div className="flex gap-2 items-center">
-                <SquarePen size={16} className="cursor-pointer" /> <Trash size={16} className="cursor-pointer" />{" "}
-              </div>
-            </div>
-
-
-        
-
-
-
-
           </div>
-        </CardContent>
 
-        <div className=""></div>
+          <Dialog>
+            <Dialog>
+  <DialogTrigger>Abrir</DialogTrigger>
+  <DialogContent>
+    <DialogHeader>
+      <DialogTitle>Editar Tarefa</DialogTitle>
+    </DialogHeader>
+
+    <div className="flex gap-2">
+      <Input />
+      <Button>Editar</Button>
+      </div>
+
+  </DialogContent>
+</Dialog>
+          </Dialog>
+
+          <div className="flex justify-between mt-4">
+            <div className="flex gap-2 items-center">
+              <ListCheck size={18} />
+              <p className="text-xs">Tarefas Concluidas (3/3) </p>
+            </div>
+            <AlertDialog>
+              <AlertDialogTrigger asChild>
+                <Button
+                  variant="outline"
+                  size="sm"
+                  className="text-xs gap-1.5 cursor-pointer"
+                >
+                  <Trash2 size={16} /> Limpar tarefas concluídas
+                </Button>
+              </AlertDialogTrigger>
+
+              <AlertDialogContent>
+                <AlertDialogHeader>
+                  <AlertDialogTitle>
+                    Tem certeza que deseja excluir x itens?
+                  </AlertDialogTitle>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                  <AlertDialogAction>Sim</AlertDialogAction>
+                  <AlertDialogCancel>Cancelar</AlertDialogCancel>
+                </AlertDialogFooter>
+              </AlertDialogContent>
+            </AlertDialog>
+          </div>
+
+          <div className="h-2 w-full bg-gray-100 mt-4 rounded-md">
+            <div
+              className="h-full w-[50%] bg-blue-500 rounded-md"
+              style={{ width: "50%" }}
+            ></div>
+          </div>
+
+          <div className="flex justify-end intems-center mt-2 gap-2">
+            <Sigma size={18} />
+            <p className="text-xs">3 Tarefas no total</p>
+          </div>
+
+          <AlertDialog>
+            <AlertDialogTrigger asChild>
+              <Button
+                variant="outline"
+                size="sm"
+                className="text-xs gap-1.5 cursor-pointer"
+              >
+                <Trash2 size={16} /> Limpar tarefas concluídas
+              </Button>
+            </AlertDialogTrigger>
+
+            <AlertDialogContent>
+              <AlertDialogHeader>
+                <AlertDialogTitle>
+                  Tem certeza que deseja excluir x itens?
+                </AlertDialogTitle>
+              </AlertDialogHeader>
+              <AlertDialogFooter>
+                <AlertDialogAction>Sim</AlertDialogAction>
+                <AlertDialogCancel>Cancelar</AlertDialogCancel>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialog>
+        </CardContent>
       </Card>
     </main>
   );
